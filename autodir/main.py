aa2bbc8049baf5be625cb4c241be0ccc83cccfe3
mitchellaha <73451514+mitchellaha@users.noreply.github.com>
2022-10-16 14:36:38 -0600

    Renamed Main autodir folder

---
+++ autodir/main.py
@@ -0,0 +1,12 @@
+from modules.directory_maker import create_directory_from_tree, create_directory_if_not_exist
+
+
+def autodir(DirStructure:list, RootFolder:str):
+    """Creates The Structured Directory from BlockList
+
+    Args:
+        DirStructure (list): The BlockList to Create
+        RootFolder (str): The Folder to Create It In
+    """
+    create_directory_if_not_exist(RootFolder)
+    create_directory_from_tree(DirStructure, RootFolder)
